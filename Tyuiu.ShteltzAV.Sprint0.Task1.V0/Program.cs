6bf4343867e4a1a717844d02b5135fc640e47c5b
Tyuiu.ShteltzAV@outlook.com <yorshik.yorsh@gmail.com>
2026-09-10 01:14:56 +0500

    Task1

---
+++ Tyuiu.ShteltzAV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.ShteltzAV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Саша";
+            int age = 17;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ". Мне " + age + " лет.");
+
+            Console.ReadLine();
+        }
+    }
+}
