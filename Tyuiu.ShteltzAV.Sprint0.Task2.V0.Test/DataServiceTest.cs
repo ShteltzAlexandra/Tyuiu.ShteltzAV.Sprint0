c779fec22ab3530c569d20ce41e58cd239df0498
Tyuiu.ShteltzAV@outlook.com <yorshik.yorsh@gmail.com>
2026-09-10 02:03:06 +0500

    Task2

---
+++ Tyuiu.ShteltzAV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ShteltzAV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ShteltzAV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Саша";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Саша", res);
+        }
+    }
+}
---
+++ Tyuiu.ShteltzAV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.ShteltzAV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.ShteltzAV.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Саша"));
+            Console.ReadLine();
+        }
+    }
+}
